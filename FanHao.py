e99e2f60911aa87629738c2978abdc8a0028b7bb
Xin Han <hh235206661@gmail.com>
2015-08-13 22:57:30 +0800

    1.0.1

--- FanHao.py
+++ FanHao.py
@@ -1,4 +1,25 @@
-#-*- coding:utf-8 -*
+#!/usr/bin/python
+#coding=utf-8
+
+__AUTOR__= "HansonHH"
+__DATA__= "13/08/15"
+__VERSAO__= "1.0.1"
+__GITHUB__= "https://github.com/HansonHH"
+
+"""
+Copyright (C) 2015  Franco Colombino
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+"""
+
+
+
 import urllib
 import urllib2
 import re 
